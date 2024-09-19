74055d78d2dbc518502866fb146e969c70291a8a
edson <edson@edson-HP-ProDesk-400-G6-Desktop-Mini-PC>
2024-09-19 11:42:50 -0700

    Versión inicial 19-sep-2024

---
+++ C-clock.X/main.c
@@ -0,0 +1,18 @@
+
+#include <xc.h>
+#include "main.h"
+#include "clock.h"
+
+void main(void) {
+    
+    /* System Clock Initialization */
+	CLOCK_Initialize();
+    
+    /* infinite loop */
+    while(1)
+    {
+
+    }
+    
+    return;
+}
